7c89e21b19e50b0bf62daaa276d0b18d2a35a450
Jan Klass <kissaki@posteo.de>
2023-11-11 18:25:09 +0100

    fix: Update Steam Community link filter URL
    * Add debug logging
    * Instead of throwing on unexpected browser behavior, warn and fall back to returning an empty result object

--- background_script.js
+++ background_script.js
@@ -1,19 +1,20 @@
+const urlFilterPrefix = 'https://steamcommunity.com/linkfilter/?u='
+const urlPattern = urlFilterPrefix + '*'
+
+console.debug('Loading…')
+
+let onBeforeRequestHandler = function(details) {
+  console.debug(`onBeforeRequest with url value '${details.url}'`)
+  if (!details.url.startsWith(urlFilterPrefix)) {
+    console.warning('Invalid operation - URL does not match expected prefix')
+    return {}
+  }
+  const targetUrl = details.url.substring(urlFilterPrefix.length).replaceAll('%3A', ':').replaceAll('%2F', '/')
+  console.debug(`Returning redirect target '${targetUrl}'…`)
+  return { redirectUrl: targetUrl }
+}
+const requestFilter = { urls: [urlPattern], types: ["main_frame"] }
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/onBeforeRequest
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    if (!details.url.startsWith('https://steamcommunity.com/linkfilter/?url=')) {
-      throw 'invalid operation'
-    }
-    const targetUrl = details.url.substring(43)
-    return { redirectUrl: targetUrl }
-  },
-  {
-    urls: [
-      "https://steamcommunity.com/linkfilter/?url=*"
-    ],
-    types: [
-      "main_frame"
-    ]
-  },
-  ["blocking"]
-)
+chrome.webRequest.onBeforeRequest.addListener(onBeforeRequestHandler, requestFilter, ["blocking"])
+
+console.debug('Loaded')
